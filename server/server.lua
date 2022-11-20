77cfb76efb21d097b36974a6cf5242d2fcd8c6c8
Matdbx10 <69509315+Matdbx10@users.noreply.github.com>
2022-11-20 08:56:41 +0100

    Patch Update

--- server/server.lua
+++ server/server.lua
@@ -10,26 +10,7 @@ end)
 --         VERSION CHECK          --
 --================================--
 
-Citizen.CreateThread( function()
-    updatePath = "/Matdbx10/mAdmin" -- your git user/repo path
-    resourceName = "mAdmin" -- the resource name
-    
-    function checkVersion(err,responseText, headers)
-        curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
-    
-        if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-            print("\n###############################")
-            print("\n"..resourceName.." is outdated, should be:\n"..responseText.."is:\n"..curVersion.."\nplease update it from https://github.com"..updatePath.."")
-            print("\n###############################")
-        elseif tonumber(curVersion) > tonumber(responseText) then
-            print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
-        else
-            print("\n"..resourceName.." is up to date, have fun!")
-        end
-    end
-    
-    PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/version", checkVersion, "GET")
-    end)
+--En DEV . . .
 
 --================================--
 --         Début du script        --
