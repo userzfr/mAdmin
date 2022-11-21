ed7d657df16017d96bfeab2a6fd7843b67ef7fc6
Matdbx10 <69509315+Matdbx10@users.noreply.github.com>
2022-11-21 20:48:07 +0100

    Color Patch

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config = {
     --Locale = 'fr', -- Language available: fr, soon
 
-    --Color = 'r', -- Language available: b (blue), r (red), g (green)
+    Color = 'r', -- Language available: b (blue), r (red), g (green)
     
     webhook = { -- LOGS DISCORD
         Staffmodeon = "DISCORD_WEBHOOK",      -- MODE STAFF ON
